47fd3d2b9363ebe52970ef07ac25f3a750f8fce2
Aprido Ilham <aprido233@gmail.com>
2025-07-31 01:13:51 +0700

    membuat role dan permission

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'role' => \Spatie\Permission\Http\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Http\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Http\Middleware\RoleOrPermissionMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
