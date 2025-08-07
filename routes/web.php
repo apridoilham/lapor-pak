a6421675543c11c2ac23063d7caa8651e494237a
Aprido Ilham <aprido233@gmail.com>
2025-08-07 13:52:39 +0700

    membuat halaman user profile

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\User\ReportController as UserReportController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\User\HomeController;
+use App\Http\Controllers\User\ProfileController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/reports', [UserReportController::class, 'index'])->name('report.index');
 Route::get('/report/{code}', [UserReportController::class, 'show'])->name('report.show');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
+});
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store'])->name('login.store');
